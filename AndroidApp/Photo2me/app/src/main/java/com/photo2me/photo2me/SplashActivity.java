27c67b6da95fd7a9bfb88638d55a6980218fa644
Fábio Thomaz Molinar <Fbio Thomaz Molinar>
2016-07-14 22:15:59 +0200

    Adicionando efeito a página do splash. Finalizado splash.

--- AndroidApp/Photo2me/app/src/main/java/com/photo2me/photo2me/SplashActivity.java
+++ AndroidApp/Photo2me/app/src/main/java/com/photo2me/photo2me/SplashActivity.java
@@ -4,19 +4,27 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_splash);
+        logo = (ImageView)findViewById(R.id.logo);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo.startAnimation(animation);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                final Intent intent = new Intent(SplashActivity.this,MainActivity.class);
                 SplashActivity.this.startActivity(intent);
                 SplashActivity.this.finish();
             }
