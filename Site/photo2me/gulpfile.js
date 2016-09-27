a11ffdd773428c9ff0405450e7b70c430f65c591
Fábio Thomaz Molinar <Fbio Thomaz Molinar>
2016-09-27 22:17:22 +0200

    Semantic configurado.

--- Site/photo2me/gulpfile.js
+++ Site/photo2me/gulpfile.js
@@ -1,11 +1,22 @@
 var
   gulp = require('gulp'),
-  semanticBuild = require('semantic-ui/tasks/build'),
-  semanticWatch = require('semantic-ui/tasks/watch');
+  requireDir = require('require-dir'),
+  semantic = requireDir('resources/assets/semantic/tasks');
 
+//Pegando o jquery da pasta node_modules e enviando para a pasta public
 gulp.task('jquery',function(){
   return gulp.src('node_modules/jquery/dist/jquery.min.js')
     .pipe(gulp.dest('public/js'));
 });
-gulp.task('semanticBuild',semanticBuild);
-gulp.task('semanticWatch',semanticWatch);
+//Tarefas do semantic dentro de resources
+gulp.task('semanticBuild',semantic.build);
+gulp.task('semanticWatch',semantic.watch);
+//Transferindo os arquivos compilados do semantic para a pasta public
+gulp.task('semanticJS',function(){
+  return gulp.src('resources/assets/semantic/dist/semantic.min.js')
+    .pipe(gulp.dest('public/js'))
+});
+gulp.task('semanticCSS',function(){
+  return gulp.src('resources/assets/semantic/dist/semantic.min.css')
+    .pipe(gulp.dest('public/css'))
+});
