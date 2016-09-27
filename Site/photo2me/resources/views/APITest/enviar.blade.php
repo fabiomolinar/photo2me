54477760b407d37fd7dbe0ee1e3c57fe490d51de
Fábio Thomaz Molinar <Fbio Thomaz Molinar>
2016-09-28 00:08:13 +0200

    Finalizado versão beta das configurações do semantic.

--- Site/photo2me/resources/views/APITest/enviar.blade.php
+++ Site/photo2me/resources/views/APITest/enviar.blade.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="{{ asset('/css/semantic.min.css') }}">
 {{-- Enviar foto para o servidor --}}
 <form class="" action="receberFoto" method="post" enctype="multipart/form-data">
   <input type="hidden" name="MAX_FILE_SIZE" value="30000">
@@ -13,3 +14,7 @@
   <input type="text" name="lastModified" value="">
   <input type="submit" value="Upload Image" name="submit">
 </form>
+Testando o semantic:
+<button class="ui primary basic button">Primary</button>
+<label>First Name</label>
+<input type="text" name="first-name" placeholder="First Name">
